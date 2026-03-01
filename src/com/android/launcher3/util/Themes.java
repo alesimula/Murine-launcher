857d775d8e31f52709dad77a1c296ecd5998f859
Alex Sim <alessandro.simula@lynxspa.com>
2026-03-01 19:41:43 +0100

    THEME - use the Launcher3's dialogCornerRadius instead of Android's

--- src/com/android/launcher3/util/Themes.java
+++ src/com/android/launcher3/util/Themes.java
@@ -68,8 +68,7 @@ public class Themes {
     }
 
     public static float getDialogCornerRadius(Context context) {
-        return getDimension(context, android.R.attr.dialogCornerRadius,
-                context.getResources().getDimension(R.dimen.default_dialog_corner_radius));
+        return context.getResources().getDimension(R.dimen.dialogCornerRadius);
     }
 
     public static float getDimension(Context context, int attr, float defaultValue) {
