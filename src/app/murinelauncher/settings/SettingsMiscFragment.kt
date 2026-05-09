a438433437aec317eebea1fec1589a7b2d63540f
Alex Sim <alessandro.simula@lynxspa.com>
2026-05-09 02:14:22 +0200

    SETTINGS - add default launcher preference

--- src/app/murinelauncher/settings/SettingsMiscFragment.kt
+++ src/app/murinelauncher/settings/SettingsMiscFragment.kt
@@ -1,5 +1,8 @@
 package app.murinelauncher.settings
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.provider.Settings
 import androidx.preference.Preference
 import app.murinelauncher.settings.common.AbstractSettingsFragment
 import com.android.launcher3.R
@@ -9,6 +12,7 @@ import com.android.launcher3.util.DisplayController
 public final class SettingsMiscFragment: AbstractSettingsFragment() {
 
     companion object {
+        const val PREF_DEFAULT_LAUNCHER: String = "pref_default_launcher"
     }
 
     override fun getPreferenceScreenResId() = R.xml.murine_prefs_misc
@@ -17,7 +21,20 @@ public final class SettingsMiscFragment: AbstractSettingsFragment() {
 
 
     override fun initPreference(preference: Preference, info: DisplayController.Info): Boolean {
+        val context = requireContext()
         when (preference.key) {
+            PREF_DEFAULT_LAUNCHER -> {
+                val pm = context.packageManager
+                val homeIntent = Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME)
+                val resolveInfo = pm.resolveActivity(homeIntent, PackageManager.MATCH_DEFAULT_ONLY)
+                preference.summary = if (resolveInfo == null || resolveInfo.activityInfo.packageName == "android") "???" else
+                    resolveInfo.loadLabel(pm).toString()
+                preference.setOnPreferenceClickListener {
+                    context.startActivity(Intent(Settings.ACTION_HOME_SETTINGS))
+                    true
+                }
+                return true
+            }
             else -> return true
         }
     }
