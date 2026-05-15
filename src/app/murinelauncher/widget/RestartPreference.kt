78f89d197164458a0a0c6d758cd5658b894cf755
Alex Sim <alessandro.simula@lynxspa.com>
2026-05-15 02:12:55 +0200

    MISC - close settings activity before restarting the launcher

--- src/app/murinelauncher/widget/RestartPreference.kt
+++ src/app/murinelauncher/widget/RestartPreference.kt
@@ -1,6 +1,8 @@
 package app.murinelauncher.widget
 
+import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.util.AttributeSet
 import android.widget.Toast
 import androidx.preference.Preference
@@ -9,6 +11,7 @@ import com.android.launcher3.Utilities
 class RestartPreference @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : Preference(context, attrs) {
     override fun onClick() {
         Toast.makeText(context, "\u003E\u2A4A\u003C", Toast.LENGTH_SHORT).show()
+        (context as? Activity ?: (context as? ContextWrapper)?.baseContext as? Activity)?.finish()
         Utilities.restart()
     }
 }
