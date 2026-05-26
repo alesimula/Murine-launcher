ffe50be201f32bdec4fc5bf13d7a65f88b7fc83d
Alex Sim <alessandro.simula@lynxspa.com>
2026-05-26 20:17:17 +0200

    DEBUGGING - secret debug crash testing

--- src/app/murinelauncher/widget/RestartPreference.kt
+++ src/app/murinelauncher/widget/RestartPreference.kt
@@ -3,12 +3,38 @@ package app.murinelauncher.widget
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
 import android.widget.Toast
 import androidx.preference.Preference
+import androidx.preference.PreferenceViewHolder
+import com.android.launcher3.BuildConfig
 import com.android.launcher3.Utilities
 
 class RestartPreference @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : Preference(context, attrs) {
+    override fun onBindViewHolder(holder: PreferenceViewHolder) {
+        super.onBindViewHolder(holder)
+        if (BuildConfig.DEBUG) {
+            // Long press three times to crash
+            var crashCountdown = 3
+            holder.itemView.setOnLongClickListener {
+                if (crashCountdown == 0) {
+                    val testException = object : RuntimeException("You") {}
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        val recoveryHandler = Thread.getDefaultUncaughtExceptionHandler()
+                        if (recoveryHandler == null) throw testException
+                        else recoveryHandler.uncaughtException(Thread.currentThread(), testException)
+                    }, 250)
+                } else {
+                    Toast.makeText(context, "${crashCountdown}...", Toast.LENGTH_SHORT).show()
+                    crashCountdown--
+                }
+                true
+            }
+        }
+    }
+
     override fun onClick() {
         Toast.makeText(context, "\u003E\u2A4A\u003C", Toast.LENGTH_SHORT).show()
         (context as? Activity ?: (context as? ContextWrapper)?.baseContext as? Activity)?.finish()
