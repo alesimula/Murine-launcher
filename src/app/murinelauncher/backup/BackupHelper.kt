e5bbe34664f0615c5ffb2dab5c9c5841af19979c
Alex Sim <alessandro.simula@lynxspa.com>
2026-08-28 12:30:12 +0200

    BACKUP - edit comment

--- src/app/murinelauncher/backup/BackupHelper.kt
+++ src/app/murinelauncher/backup/BackupHelper.kt
@@ -32,7 +32,7 @@ object BackupHelper {
     private const val TMP_PREFS = "murine_backup_tmp_prefs"
     private const val DOWNGRADE_JSON = "downgrade_schema.json"
     private const val MAIN_PREFS_XML = LauncherFiles.SHARED_PREFERENCES_KEY + ".xml"
-    // Current implementation only supports level 3 (default) and 4
+    // Current implementation only supports level 1 to 4, default is 3
     private const val ZSTD_COMPRESSION_LEVEL = 3
 
     /** Backed up prefs; IMPORTANT: also check backupscheme.xml **/
